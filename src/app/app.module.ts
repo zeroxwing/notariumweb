03469ce000ba7ac9bb093732527d4ee4e8a4524f
zeroxwing <juan.fernandezs@mail.udp.cl>
2023-03-19 20:38:53 -0300

    first version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,46 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from "@angular/material/input";
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from "@angular/material/button";
+import { HttpClientModule } from '@angular/common/http';
+import {AppRoutingModule, routingComponents} from './app-routing.module';
+import {ReactiveFormsModule} from "@angular/forms";
+import {MatMenuModule} from "@angular/material/menu";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import { CrearRepertorioComponent } from './crear-repertorio/crear-repertorio.component';
+import {MatDatepickerModule} from "@angular/material/datepicker";
+import {MatNativeDateModule} from "@angular/material/core";
+import { ListarRepertoriosComponent } from './listar-repertorios/listar-repertorios.component';
+import {MatTableModule} from "@angular/material/table";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    routingComponents,
+    CrearRepertorioComponent,
+    ListarRepertoriosComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    HttpClientModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
